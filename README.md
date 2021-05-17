eaa6fece5001d9f069927efc3d04d77f334d90a5
krs131099 <krs.nandhan999@gmail.com>
2021-05-17 11:12:14 +0530

    added files

---
+++ README.md
@@ -0,0 +1,85 @@
+# SIMPLE CALCULATOR api
+
+@author:* K R S Nandhan.*
+
+This is a simple calcluator api, user can call the api with a expression as a input parameter and,
+
+gets the answer for the expression in JSON format. This program was written in Python using Flask.
+
+
+
+### Some assumptions were made in making this program:
+
+* This calculator can only perfom operations of addition, subtration, multiplication, and division.
+* Scientific Expressions are not handled by this calculator.
+* Brackets are neglected due to complex expressions.
+* The first element is always a positive number.
+* Number of operators should be sufficient for operands.
+* The expression should be encoded before calling api.
+* All integers are considered as unsigned.
+
+
+### Approach:
+1. First, a raw quoted string is unquoted and any white spaces around string is trimmed.
+2. Check that, expression does not violate assumptions.
+3. Using regular expressions, list of operators and operands was extracted.
+4. With BODMAS rule to calculate expressions,
+   ( i). First all the divisions were made.
+   (ii). Next all the divisions were made.
+   (iii).Perform addition or subtraction on the remaining elements.
+   (iv). Return result in json format.
+
+
+### api information
+
+GET url/calc
+Performs the calculation and returns answer in json format.
+
+Input : "expression" -> string containing the expression
+
+Output : dictionary in json format.
+    "Answer"    -> Contains answer if the input expression is valid.
+    "Message"   -> Happy message.
+    "Error"     -> If the expression is invalid, this key is generated.
+    "Reason"    -> Explanation for the Error.
+
+
+### api usage
+
+**NOTE : Please read the assumptions before performing calculaitons**
+**NOTE : Please encode the expression before sending request manually.**
+examples:
+(i). Expression = "12+3"
+    `GET url/calc?expression=12%2B3` 
+
+    `{
+        "Answer"    =   15.0,
+        "Message"   =   "Happy Calculations"
+    }`
+
+(ii).Expression = "100 + 58 * 13/13"
+    `GET url/calc?expression=100%20%2B%2058%20%2A%2013%20/13`
+
+    `{
+        "Answer"    =   158.0 ,
+        "Message"   =   "Happy Calculations"
+    }`
+
+(iii). Expression = (100+2)
+    `GET url/calc?expression=%28100%2B2%29`
+
+    `{
+        "Error"     =   "Invalid Expression",
+        "Reason"    =   "Expression does not starts or ends with digit"
+    }`
+
+(iv). Expression = 100 58 * 13
+    `GET url/calc?expression=100%2058%20%2A%2013`
+
+    `{
+        "Error"     =   "Invalid Expression",
+        "Reason"    =   "Number of Operators are not matching to perform operation."
+    }`
+
+
+*If given extra time, support for complex expressions is implemented.*
---
+++ calculator.py
@@ -0,0 +1,116 @@
+import re
+import flask
+from flask import jsonify, request
+import urllib.parse
+
+
+# NOTE :: This calculator program was built under some assumptions.
+# Kindly refer, README file for more information.
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/calc', methods=['GET'])
+def calculator():
+
+    
+    # extract required data and unquote(decode) string.
+    expression = urllib.parse.unquote(request.args.get('expression')).strip()
+
+
+    if len(re.findall(r"^\d.+\d$",expression)) == 0:
+        # expression does not starts or ends with numbers
+        output = {  "Error":"Invalid Expression",
+                    "Reason":"Expression does not starts or ends with number"
+                }
+        return jsonify(output)
+    
+
+    #preprocess and get operators and operands
+    operators = re.findall(r"[+\-*/]",expression)
+    operands = re.findall(r"[0-9]+", expression)
+
+    
+    if len(operators)==0 or len(operands)==0:
+        output = {  "Error": "Invalid Expression",
+                    "Reason":"Does not contain operators or operands"
+                }
+
+
+    if len(operators)+1 != len(operands):
+        # Expression is invalid
+        output = {"Error": "Invalid Expression",
+                "Reason":"Number of Operators are not matching to perform operations."
+                }
+        return jsonify(output)
+
+    
+    # Using BODMAS rule,
+    # First preference is for '/', followed by '*', then '+', and '-'
+
+    # count nubmer of multiplication operators and division operators
+    mul_count = operators.count('*')
+    div_count = operators.count('/')
+
+
+    # compute all divisions
+    while div_count:
+        
+        # get index of '/' for popping operands
+        pos = operators.index('/')
+        
+        # get two operands at the position of '/'
+        a = float(operands.pop(pos))
+        b = float(operands.pop(pos))
+        
+        # insert the result at position
+        operands.insert(pos, a/b)
+
+        # eliminate operator
+        operators.pop(pos)
+        
+        div_count -= 1
+    
+    
+    while mul_count:
+
+        # get index of '*' for popping operands
+        pos = operators.index('*')
+        
+        # get operands at position of '*'
+        a = float(operands.pop(pos))
+        b = float(operands.pop(pos))
+        
+        # insert result at pos
+        operands.insert(pos, a*b)
+
+        #eliminate operator
+        operators.pop(pos)
+
+        mul_count -= 1
+
+
+    # Calculate additions and subtractions
+    for x in (operators):
+
+        a = float(operands.pop(0))
+        b = float(operands.pop(0))
+        
+        #
+        if x == '+':    
+            operands.insert(0,a+b)
+        else:
+            operands.insert(0,a-b)
+
+    # operands list then contains one item, which is the final result.
+
+    output = {}
+    output['Answer'] = operands[0]
+    output['Message'] = "Happy Calculations"
+    
+    # return output in json format
+    return jsonify(output)
+
+app.run()
